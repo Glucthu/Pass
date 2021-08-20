8230a3eeddc0b5a7f960d1d127563263de76efd7
glucthu <glucthu@protonmail.com>
2021-08-20 13:17:11 -0300

    changed web page

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function showCoords(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    var coords = "X: " + x + ", Y: " + y;
-    document.getElementById("coords").innerHTML = coords;
-  }
+function RandomPassGen(){
+    let length = document.getElementById("length").value;
+    let pass = "";
+    let min = 33, max = 127;
+
+    for(let i = 0; i < length; i++){
+        pass += String.fromCharCode((Math.floor((Math.random() * (max - min))+ min)));
+    }
+    
+    document.getElementById("pass").innerHTML = pass;
+}
