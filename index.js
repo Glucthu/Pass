be73f98db655da4b3f0a4f59711166c8298a82cf
Glucthu <53957965+Glucthu@users.noreply.github.com>
2022-02-10 14:12:40 -0300

    added style

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ function RandomPassGen(){
     let pass = "";
     let min = 33, max = 127;
 
-    for(let i = 0; i < length; i++){
-        pass += String.fromCharCode((Math.floor((Math.random() * (max - min))+ min)));
+    if(length >= 1 && length <= 64){
+        for(let i = 0; i < length; i++){
+            let newChar = String.fromCharCode((Math.floor((Math.random() * (max - min))+ min)));
+            
+            if (newChar == "<") newChar = "&lt;";
+            if (newChar == ">") newChar = "&gt;";
+
+            pass += newChar;
+        }
+        
+        document.getElementById("pass").innerHTML = pass;
+
+        document.getElementById("pass").style.overflowX = "scroll";
+        document.getElementById("pass").style.border = "1px solid #222";
+
+        document.getElementById("copy").style.visibility = "visible";
     }
-    
-    document.getElementById("pass").innerHTML = pass;
+}
+
+function CopyPass(){
+    navigator.clipboard.writeText(document.getElementById("pass").innerHTML);
 }
